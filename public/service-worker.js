abf12b0432cdabc7ca6c265b873565640a446c28
Valerie Riley <valeriesriley@gmail.com>
2020-04-10 15:13:35 -0600

    heroku debug

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,7 +9,7 @@ const FILES_TO_CACHE = [
     "/icons/icon-512x512.png"
 ];
 
-const CACHE_NAME = "static-cache-v1";
+const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", function (e) {
