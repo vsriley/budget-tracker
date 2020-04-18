aeb92a70116da3647111a0d32085200b138f98b9
Valerie Riley <valeriesriley@gmail.com>
2020-04-17 22:54:56 -0600

    heroku debug

--- server.js
+++ server.js
@@ -16,7 +16,7 @@
   app.use(express.static("public"));
   
   // mongoose setup here
-  mongoose.connect("mongodb://localhost/budget",{
+  mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget",{
     useNewUrlParser: true,
     useFindAndModify: false
   }); 
