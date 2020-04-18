edcc464fc63e4d960fa93c919481c8b7c0ff0576
Valerie Riley <valeriesriley@gmail.com>
2020-04-17 20:52:47 -0600

    heroku setup

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -1,4 +1,10 @@
 let db;
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
 
 const request = indexedDB.open("budget", 1);
 
--- server.js
+++ server.js
@@ -1,24 +1,11 @@
-  // import homeRoute from "./routes/index";
-
   const express = require("express");
   const logger = require("morgan");
   const mongoose = require("mongoose");
   const compression = require("compression");
-  // const path = require ('path');
-  // require("dotenv").config();
   
   const PORT = 3000 || process.env.PORT;
   
   const app = express();
-  
-  // app.use(express.static(__dirname));
-  // app.use(express.static(path.join(__dirname, 'build')));
-  // app.get('/ping', function (req, res) {
-  //   return res.send('ping');
-  // });
-  // // app.get('/*', function (req, res) {
-  // //   res.sendFile(path.join(__dirname, 'build', 'index.html')); //serving build folder
-  // // });
 
   app.use(logger("dev"));
   
@@ -33,20 +20,10 @@
     useNewUrlParser: true,
     useFindAndModify: false
   }); 
-  // mongoose.Promise = global.Promise;
   
-  // // routes
-  // homeRoute(app);
+  // routes
   app.use(require("./routes/api.js"));
-  // // set up a wildcard route to catch related endpoints and outputs a response.
-  // app.get('*', (req, res) => {
-  //   res.status(400).json({
-  //     message: 'This is Project Support. Please see documentation for the proper routes.',
-  //   });
-  // });
   
   app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
-
-// export default app;
