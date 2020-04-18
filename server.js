bb2897a4164aa45d03b1c27c5ea4827f3167f596
Valerie Riley <valeriesriley@gmail.com>
2020-04-17 20:39:02 -0600

    heroku setup

--- server.js
+++ server.js
@@ -26,7 +26,7 @@
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   
-  app.use(express.static("/public"));
+  app.use(express.static("public"));
   
   // mongoose setup 
   mongoose.connect(process.env.MONGODB_URI|| "mongodb://localhost/budget",{
